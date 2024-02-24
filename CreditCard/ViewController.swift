454d77cd466c4f4cbaa9e418b456fe53f60b05c1
Ibrahim Nasser <thenasserr@gmail.com>
2024-02-24 17:59:43 +0200

    Initial Commit

---
+++ CreditCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreditCard
+//
+//  Created by Ibrahim Nasser Ibrahim on 24/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
